15652c7334f126f07a52c99ec581256225546697
이지은 <ljieun@ijieun-ui-MacBookPro.local>
2020-10-23 16:05:11 +0900

    Updates

---
+++ precache-manifest.bd377291b27f2d77234c2f724ca1313f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d781c94e39dbf408bc5384733d0e30f4",
+    "url": "/jwitter/index.html"
+  },
+  {
+    "revision": "8574723af1143e3f4721",
+    "url": "/jwitter/static/js/2.204b091f.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/jwitter/static/js/2.204b091f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5429623648f819ee04e",
+    "url": "/jwitter/static/js/main.9d604648.chunk.js"
+  },
+  {
+    "revision": "d27a12203f970ffcd255",
+    "url": "/jwitter/static/js/runtime-main.3f2c118c.js"
+  }
+]);
